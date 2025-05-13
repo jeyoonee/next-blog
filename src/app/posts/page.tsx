459b6648e6cf8330b34bc656332ab10c2e5f6c6e
jeyoonee <jae4419@naver.com>
2025-05-13 18:05:27 +0900

    [feat] Add category filter to Posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import Dropdown from "react-dropdown";
+import postData from "../../../data/posts.json";
+import PostPreview from "../components/PostPreview";
+import Image from "next/image";
+import { PostPreviewData } from "@/types/post";
+import { useState } from "react";
+
+const options = ["all", "my story", "frontend", "backend", "javascript"];
+
 export default function Posts() {
-  return <h1>게시글들...</h1>;
+  const [option, setOption] = useState("all");
+  const filteredPosts =
+    option === "all"
+      ? postData
+      : postData.filter((post: PostPreviewData) => post.category === option);
+
+  return (
+    <>
+      <div className="mb-4 w-60">
+        <Dropdown
+          options={options}
+          value={option}
+          onChange={(e) => {
+            setOption(e.value);
+          }}
+        />
+      </div>
+      {filteredPosts.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {filteredPosts.map((item, id) => (
+            <PostPreview data={item} key={id} />
+          ))}
+        </div>
+      ) : (
+        <p>No posts available in this category.</p>
+      )}
+    </>
+  );
 }
