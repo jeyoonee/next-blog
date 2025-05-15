276c536b4b1f4eb7895127fabe7b6d96c328076c
jeyoonee <jae4419@naver.com>
2025-05-15 14:52:45 +0900

    [feat] finalize layout in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,27 +2,43 @@
 
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 const markdown = `
-💻 *Driven Frontend Engineer* with 2.5 years of experience in building scalable, user-centric web applications using TypeScript. Specialized in responsive UI/UX and cross-functional collaboration.
+💻 *Driven Frontend Engineer* with 2.5 years of experience in building scalable, user-centric web applications using TypeScript. Specialized in responsive UI/UX and cross-functional collaboration.  
+<br />
+<br />
 
-📍 **Key Achievements:**
-
-- *Led UI/UX development* for a news app platform with 20K+ monthly users. Boosted user satisfaction by implementing responsive layouts and reducing initial load time by 30%.
-- *Collaborated closely* with designers and backend engineers to deliver accessible and responsive interfaces.
-- *Adapted swiftly* to production issues and enhanced user-facing features based on direct feedback.
-
+📍 **Key Achievements**
+<br />
+<br />
+- **Led UI/UX development** for a news app platform with 20K+ monthly users. Boosted user satisfaction by implementing responsive layouts and reducing initial load time by 30%.
+  <br />
+  <br />
+  **Collaborated closely** with designers and backend engineers to deliver accessible and responsive interfaces.
+  <br />
+  <br />
+- **Adapted swiftly** to production issues and enhanced user-facing features based on direct feedback.
+  <br />
+  <br />
 ✨ I thrive in opportunities where I can apply my skills in *Next.js* and *React.js* to create impactful, user-centered experiences. I’m especially drawn to roles that encourage innovation and teamwork within dynamic environments.
-
+  <br />
+  <br />
 🌎 With a diverse background of staying in Europe, Thailand, and India, I bring a global perspective to my work—fostering empathy, adaptability, and a multicultural approach to problem-solving.
-
+  <br />
+  <br />
 👋 I love connecting with new people. You can reach me at wpdbs4419@gmail.com. Feel free to explore my work on GitHub or visit my blog for more insights.
 `;
 
 export default function About() {
   return (
-    <div className="prose max-w-none p-4">
-      <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+    <div className="px-10 mb-8">
+      <h1 className="text-[24px] font-bold mb-8">About Me</h1>
+      <div className="prose max-w-none">
+        <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+          {markdown}
+        </Markdown>
+      </div>
     </div>
   );
 }
