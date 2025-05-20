a22ecd539df296d5dd2a83616bbe030322763b26
jeyoonee <jae4419@naver.com>
2025-05-20 20:49:43 +0900

    [fix] issue causing build failure

--- src/app/components/MarkdownViewer.tsx
+++ src/app/components/MarkdownViewer.tsx
@@ -1,6 +1,5 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import "katex/dist/katex.min.css";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
